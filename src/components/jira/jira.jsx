39d3d82de994bc2e13479fd3700863273872b980
mantuok <mantuok@gmail.com>
2021-10-22 00:08:41 +0300

    Dashboard WIP

--- src/components/jira/jira.jsx
+++ src/components/jira/jira.jsx
@@ -1,21 +1,22 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import {ActionCreator} from '../../store/action';
 import classnames from 'classnames';
 import {
-  StepStatus,
   Step
 } from '../../const';
 import {
   isStepSuccess,
   isStepFailed
 } from '../../common/utils';
+import {getHumanDateFormat} from '../../common/day';
 
 const Jira = (props) => {
   const {id} = props;
   const dispatch = useDispatch();
   const currentJiraStatus = useSelector((state) => state.jiraStatus);
   const lastUpdated = useSelector((state => state.jiraData.nodeUpdated));
+  const lastUpdatedHumanFormat = getHumanDateFormat(lastUpdated);
   const number = useSelector((state => state.jiraData.number));
 
   const stepNameClass = classnames(
@@ -33,7 +34,7 @@ const Jira = (props) => {
       className="steps__step steps__step--jira step"
       onClick={stepClickHandler}
     >
-      <span className="step__last-update">{lastUpdated}</span>
+      <span className="step__last-update">{lastUpdatedHumanFormat}</span>
       <span className={stepNameClass}>Jira item {number}</span>
     </li>
   )
